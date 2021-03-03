75cb97f8f9f6a8f81a563f907dd9f2590f012a8a
Tim Quimbo <tquimbo1@gmail.com>
2021-03-03 17:20:09 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,8 +1,15 @@
-def hello_t(block)
-block = []
-block.each |piece| do
-puts "hey"
+def hello_t(array)
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
-end
-
 # call your method here!
